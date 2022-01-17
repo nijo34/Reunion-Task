18fdb6bb626880797384461c589dcbbc8edcc695
Nijo Noble <nijo.noble34@gmail.com>
2022-01-17 08:09:01 +0530

    Production changes

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -7,7 +7,7 @@ const prisma = new PrismaClient()
 const auth = async ( req , res , next) =>{
     try {
         const token = req.header('Authorization').replace('Bearer ' , '')
-        const decoded = jwt.verify(token , JWT_SECRET)
+        const decoded = jwt.verify(token , '70714fc0c04294b06153343454c68fc9544b1b8d8050365f89529e0b67b127020d122de259e1d3c622c4c7cf4d7c7a9a7c15fc5b0445d9481c415f6a3411ecdd')
         const user = await prisma.user.findUnique({where : {email : decoded.email }})
         
         if(!user){
